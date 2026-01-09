9c77b51d37bc0c087718acd8e4534396ced32fcb
Marco Stellino <marcoste2007@gmail.com>
2026-01-09 08:18:58 +0100

    Primo test di Faker

--- flask/demo-flask/main.py
+++ flask/demo-flask/main.py
@@ -1,12 +1,33 @@
 from flask import Flask
+from faker import Faker
+
+fake = Faker()
+
+data = []
 
 app = Flask(__name__)
+
+
+def riempi_dati ():
+    data = []
+    for _ in range(100):
+        persona = {
+            "name": fake.name(),
+            "address": fake.address(),
+            "email": fake.email()
+        }
+        data.append(persona)
+    return data
 
 @app.route("/")
 def hello_world():
     out = {
-        "message": "Hello, World!"
+        "message": "Hello, World!",
     }
     return out
+
+@app.route("/data")
+def get_data():
+    return {"data": riempi_dati()}
 
 app.run("0.0.0.0", port=11000, debug=True)
